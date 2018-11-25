787d684f766c80dda6048fa3222d1e00f5669991
julien.hagestedt <julien.hagestedt@gmail.com>
2018-11-25 21:34:37 +0100

    disable JMS and hazelcast

--- app/src/main/java/com/example/ping/PingApplication.java
+++ app/src/main/java/com/example/ping/PingApplication.java
@@ -5,17 +5,13 @@ import java.util.Arrays;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.core.env.Environment;
 import org.springframework.core.env.SimpleCommandLinePropertySource;
-import org.springframework.jms.annotation.EnableJms;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Slf4j
-@EnableJms
-@EnableCaching
 @EnableScheduling
 @EnableEurekaClient
 @EnableFeignClients
