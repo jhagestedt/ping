fab28f9a1e4fa8ed0523c402c3c20263a4ff39df
julien.hagestedt <julien.hagestedt@gmail.com>
2018-11-27 15:03:10 +0100

    enable caching

--- app/src/main/java/com/example/ping/PingApplication.java
+++ app/src/main/java/com/example/ping/PingApplication.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.core.env.Environment;
@@ -12,6 +13,7 @@ import org.springframework.core.env.SimpleCommandLinePropertySource;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Slf4j
+@EnableCaching
 @EnableScheduling
 @EnableEurekaClient
 @EnableFeignClients
